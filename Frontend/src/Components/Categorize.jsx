1fe715154129111e3d4c93f2dfe26935c9cfd1a3
bharath132 <chidambaramb2@gmail.com>
2025-01-20 20:45:10 +0530

    enhance category

--- Frontend/src/Components/Categorize.jsx
+++ Frontend/src/Components/Categorize.jsx
@@ -4,7 +4,74 @@ export const Categorize = () => {
   return (
     <div className="Categorize">
       <div className="title">
-        <h1>Categorize</h1>
+        <h1>Manage Categorize</h1>
+      </div>
+      <div className="center-wrapper">
+        <div className="Categorize_header">
+          <button className="addbtn">+ Add Categorize</button>
+        </div>
+        <div className="category_container">
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Salary</h3>
+              <h5 className="type income">Income</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Food</h3>
+              <h5 className="type">Expense</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Salary</h3>
+              <h5 className="type income">Income</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Food</h3>
+              <h5 className="type">Expense</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Salary</h3>
+              <h5 className="type income">Income</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Food</h3>
+              <h5 className="type">Expense</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- Frontend/src/Components/Transaction.jsx
+++ Frontend/src/Components/Transaction.jsx
@@ -6,6 +6,74 @@ export const Transaction = () => {
       <div className="title">
         <h1>Transaction</h1>
       </div>
+      <div className="Transactiom--con">
+      <div className="Transactiom--table">
+          <div className="trans-item">
+            <div className="Trans-info">
+              <div className="trans-title">
+                <h3>netflix</h3>
+                <h3 className="type">Type</h3>
+              </div>
+              <div className="tans-desc">
+              <h3>Salary</h3>
+              
+              <h3>24/02/2000</h3>
+              </div>
+            </div>
+            <div className="trans-amount">
+              <p>+90000</p>
+            </div>
+          </div>
+          <div className="action-btns">
+            <i class="fa-solid fa-pen-to-square edit"></i>
+            <i class="fa-solid fa-trash del"></i>
+          </div>
+        </div>
+        <div className="Transactiom--table">
+          <div className="trans-item">
+            <div className="Trans-info">
+              <div className="trans-title">
+                <h3>netflix</h3>
+                <h3 className="type income" >income</h3>
+              </div>
+              <div className="tans-desc">
+              <h3>Salary</h3>
+              
+              <h3>24/02/2000</h3>
+              </div>
+            </div>
+            <div className="trans-amount">
+              <p>+90000</p>
+            </div>
+          </div>
+          <div className="action-btns">
+            <i class="fa-solid fa-pen-to-square edit"></i>
+            <i class="fa-solid fa-trash del"></i>
+          </div>
+        </div>
+        <div className="Transactiom--table">
+          <div className="trans-item">
+            <div className="Trans-info">
+              <div className="trans-title">
+                <h3>netflix</h3>
+                <h3 className="type">Type</h3>
+              </div>
+              <div className="tans-desc">
+              <h3>Salary</h3>
+              
+              <h3>24/02/2000</h3>
+              </div>
+            </div>
+            <div className="trans-amount">
+              <p>+90000</p>
+            </div>
+          </div>
+          <div className="action-btns">
+            <i class="fa-solid fa-pen-to-square edit"></i>
+            <i class="fa-solid fa-trash del"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
