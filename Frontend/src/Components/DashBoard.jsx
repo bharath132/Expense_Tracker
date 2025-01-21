6535fab4306d75e923dce81a19206d8f3aeb52cc
bharath132 <47454425+bharath132@users.noreply.github.com>
2025-01-22 01:28:50 +0530

    fixing responsive

--- Frontend/src/Components/DashBoard.jsx
+++ Frontend/src/Components/DashBoard.jsx
@@ -38,6 +38,55 @@ export const DashBoard = () => {
       </div>
       <div className="dash-chart-1"></div>
       <div className="dash-chart-2"></div>
+      <div className="receent_traans">
+        <h1>Recent Transcation</h1>
+        <div className="recent-container">
+        <div className="Transactiom--table">
+            <div className="trans-item">
+              <div className="Trans-info">
+                <div className="trans-title">
+                  <h3>netflix</h3>
+                  <h3 className="type">Type</h3>
+                </div>
+                <div className="tans-desc">
+                  <h3>Salary</h3>
+
+                  <h3>24/02/2000</h3>
+                </div>
+              </div>
+              <div className="trans-amount">
+                <p>+90000</p>
+              </div>
+            </div>
+            <div className="action-btns">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="Transactiom--table">
+            <div className="trans-item">
+              <div className="Trans-info">
+                <div className="trans-title">
+                  <h3>netflix</h3>
+                  <h3 className="type">Type</h3>
+                </div>
+                <div className="tans-desc">
+                  <h3>Salary</h3>
+
+                  <h3>24/02/2000</h3>
+                </div>
+              </div>
+              <div className="trans-amount">
+                <p>+90000</p>
+              </div>
+            </div>
+            <div className="action-btns">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
