4e635803ea5c2237117b3938ab2e4f9040119216
bharath132 <47454425+bharath132@users.noreply.github.com>
2025-01-23 20:52:30 +0530

    update category section

--- Frontend/src/Components/Categorize.jsx
+++ Frontend/src/Components/Categorize.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/Categorize.css";
+import { CategorizedItemsList } from "./CategorizedItemsList";
 export const Categorize = () => {
+  const [edit,setedit]=useState(true)
   return (
     <div className="Categorize">
       <div className="title">
@@ -10,68 +12,7 @@ export const Categorize = () => {
         <div className="Categorize_header">
           <button className="addbtn">+ Add Categorize</button>
         </div>
-        <div className="category_container">
-          <div className="categorize--item">
-            <div className="row-info">
-              <h3>Salary</h3>
-              <h5 className="type income">Income</h5>
-            </div>
-            <div className="action">
-              <i class="fa-solid fa-pen-to-square edit"></i>
-              <i class="fa-solid fa-trash del"></i>
-            </div>
-          </div>
-          <div className="categorize--item">
-            <div className="row-info">
-              <h3>Food</h3>
-              <h5 className="type">Expense</h5>
-            </div>
-            <div className="action">
-              <i class="fa-solid fa-pen-to-square edit"></i>
-              <i class="fa-solid fa-trash del"></i>
-            </div>
-          </div>
-          <div className="categorize--item">
-            <div className="row-info">
-              <h3>Salary</h3>
-              <h5 className="type income">Income</h5>
-            </div>
-            <div className="action">
-              <i class="fa-solid fa-pen-to-square edit"></i>
-              <i class="fa-solid fa-trash del"></i>
-            </div>
-          </div>
-          <div className="categorize--item">
-            <div className="row-info">
-              <h3>Food</h3>
-              <h5 className="type">Expense</h5>
-            </div>
-            <div className="action">
-              <i class="fa-solid fa-pen-to-square edit"></i>
-              <i class="fa-solid fa-trash del"></i>
-            </div>
-          </div>
-          <div className="categorize--item">
-            <div className="row-info">
-              <h3>Salary</h3>
-              <h5 className="type income">Income</h5>
-            </div>
-            <div className="action">
-              <i class="fa-solid fa-pen-to-square edit"></i>
-              <i class="fa-solid fa-trash del"></i>
-            </div>
-          </div>
-          <div className="categorize--item">
-            <div className="row-info">
-              <h3>Food</h3>
-              <h5 className="type">Expense</h5>
-            </div>
-            <div className="action">
-              <i class="fa-solid fa-pen-to-square edit"></i>
-              <i class="fa-solid fa-trash del"></i>
-            </div>
-          </div>
-        </div>
+        {edit?<CategorizedItemsList />:""}
       </div>
     </div>
   );
---
+++ Frontend/src/Components/CategorizedItemsList.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export const CategorizedItemsList = () => {
+  return (
+    <div className="category_container">
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Salary</h3>
+              <h5 className="type income">Income</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Food</h3>
+              <h5 className="type">Expense</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Salary</h3>
+              <h5 className="type income">Income</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Food</h3>
+              <h5 className="type">Expense</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Salary</h3>
+              <h5 className="type income">Income</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+          <div className="categorize--item">
+            <div className="row-info">
+              <h3>Food</h3>
+              <h5 className="type">Expense</h5>
+            </div>
+            <div className="action">
+              <i class="fa-solid fa-pen-to-square edit"></i>
+              <i class="fa-solid fa-trash del"></i>
+            </div>
+          </div>
+        </div>
+  )
+}
