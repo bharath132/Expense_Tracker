e7312e2a5d58b80520d006f2131b71a01da7baac
bharath132 <47454425+bharath132@users.noreply.github.com>
2025-03-16 11:10:19 +0530

    tesing express

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,1 +1,15 @@
-import express;
+const express=require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('HELLO WORLD')
+})
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+  })
+  app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+  })
+app.listen(3000,()=>{
+    console.log("listening")
+})
