f0913729fddf415b7757e9e0092fcdc6eefc392c
bharath132 <47454425+bharath132@users.noreply.github.com>
2025-04-12 23:41:16 +0530

    home page add function to balance , income , expense

--- Frontend/src/Components/DashBoard.jsx
+++ Frontend/src/Components/DashBoard.jsx
@@ -1,20 +1,46 @@
-import React, { useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts';
-import { LayoutDashboard, Receipt, PieChart, Settings, LogOut, Plus } from 'lucide-react';
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
 import { TranscationItemsList } from "./TranscationItemsList";
-let Balance;
-let Income;
-let Expense;
+import axios from "axios";
+let Balance = 0;
+let Income = 0;
+let Expense =0 ;
 
 export const DashBoard = () => {
-useEffect()
   const chartData = [
-    { month: 'Jan', amount: 1100 , expense : 100},
-    { month: 'Feb', amount: 1400 , expense : 200},
-    { month: 'Mar', amount: 1200 , expense : 100},
-    { month: 'Apr', amount: 1300 , expense : 400},
-    { month: 'May', amount: 1500 , expense : 300}
+    { month: "Jan", amount: 1100, expense: 100 },
+    { month: "Feb", amount: 1400, expense: 200 },
+    { month: "Mar", amount: 1200, expense: 100 },
+    { month: "Apr", amount: 1300, expense: 400 },
+    { month: "May", amount: 1500, expense: 300 },
   ];
+  const [TransactiomList, setTransactiomList] = useState([]);
+  useEffect(() => {
+    axios.post("http://localhost:3000/getTranscationList").then((res) => {
+      setTransactiomList(res.data);
+      console.log(res.data);
+    });
+  }, []);
+  TransactiomList.map((list) => {
+    if (list.type==='expense') {
+
+      Expense+= Number(list.amount)
+      console.log(Expense)
+    }
+    else{
+      Income += Number(list.amount)
+      console.log(Income)
+    }
+  });
+
   return (
     <div className="main-section">
       <div className="title">
@@ -28,7 +54,7 @@ useEffect()
           </div>
           <div className="dash_desc">
             <h1>Balance</h1>
-            <p>$11000</p>
+            <p>${Income-Expense}</p>
           </div>
         </div>
         <div className="dash_box box2">
@@ -37,7 +63,7 @@ useEffect()
           </div>
           <div className="dash_desc">
             <h1>Income</h1>
-            <p>$11000</p>
+            <p>${Income}</p>
           </div>
         </div>
         <div className="dash_box box3">
@@ -46,25 +72,35 @@ useEffect()
           </div>
           <div className="dash_desc">
             <h1>Expense</h1>
-            <p>$11000</p>
+            <p>${Expense}</p>
           </div>
         </div>
       </div>
       <div className="dash-chart-1">
-      <ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer width="100%" height="100%">
           <LineChart data={chartData}>
             <XAxis dataKey="month" />
             <YAxis />
             <Tooltip />
-            <Line type="monotone" dataKey="amount" stroke="#2563eb" strokeWidth={2} />
-            <Line type="monotone" dataKey="expense" stroke="red" strokeWidth={4} />
+            <Line
+              type="monotone"
+              dataKey="amount"
+              stroke="#2563eb"
+              strokeWidth={2}
+            />
+            <Line
+              type="monotone"
+              dataKey="expense"
+              stroke="red"
+              strokeWidth={4}
+            />
           </LineChart>
         </ResponsiveContainer>
       </div>
       <div className="dash-chart-2"></div>
       <div className="receent_traans">
         <h1>Recent Transcation</h1>
-        <TranscationItemsList/>
+        <TranscationItemsList />
       </div>
     </div>
   );
