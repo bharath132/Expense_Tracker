e68872a3462c84936b5df0d29de676f8a14bdae5
bharath132 <47454425+bharath132@users.noreply.github.com>
2025-04-28 23:38:09 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,7 +8,6 @@ app.use(express.json());
 const corsOptions = {
   origin: [
     "https://expense-tracker-frontend-one-sooty.vercel.app", // Frontend URL
-    "http://localhost:3000", // Allow localhost for development
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
