790e882705977596c841cf28fcdfb5a6e6635e29
bharath132 <47454425+bharath132@users.noreply.github.com>
2025-04-28 21:40:35 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -67,6 +67,4 @@ app.delete('/deleteCategoryList',(req,res)=>{
   const id=req.body
   CategoryList.deleteOne({_id:new mongoose.Types.ObjectId(id)})
 })
-app.listen(3000,()=>{
-    console.log("listening")
-})
+module.exports = app;
